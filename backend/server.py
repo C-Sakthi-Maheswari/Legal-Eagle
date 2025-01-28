ff0490e00e680563b291b8fd1be6a1dac7b353a0
Sheeba Sharon <sheebasharon@Sheebas-MacBook-Air.local>
2025-01-28 18:46:09 +0530

    changed server.py

--- backend/server.py
+++ backend/server.py
@@ -13,7 +13,7 @@ pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'  # Adjust as necess
 
 
 API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
-headers = {"Authorization": "Bearer "}
+headers = {"Authorization": "Bearer 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"}
 
 
 
